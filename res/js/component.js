853e85fa3663e0843ae1cb413cdc7a8ad6a934f8
olafecorcam <olafecorcam@gmail.com>
2014-02-06 15:46:28 -0200

    Pie 3D

--- res/js/component.js
+++ res/js/component.js
@@ -2,10 +2,34 @@ sap.designstudio.sdk.Component
 		.subclass(
 				"com.leandro.gp.pietd.pietd",
 				function() {
+					var that = this;
 					var saveDataResultSet = null;
 					var _corseparador = null;
 					var _angulo = null;
-					
+					var _depth = null;
+					var _starteffect = null;
+
+					var _clickedText = null;
+					var _clickedValue = null;
+
+					this.clickedText = function(value) {
+						if (value === undefined) {
+							alert(_clickedText);
+							return _clickedText;
+						} else {
+
+							return this;
+						}
+					};
+
+					this.clickedValue = function(value) {
+						if (value === undefined) {
+							return _clickedValue;
+						} else {
+
+							return this;
+						}
+					};
 					this.dataResultSet = function(value) {
 						if (value === undefined) {
 							return saveDataResultSet;
@@ -14,7 +38,22 @@ sap.designstudio.sdk.Component
 							return this;
 						}
 					};
-					
+					this.starteffect = function(value) {
+						if (value === undefined) {
+							return _starteffect;
+						} else {
+							_starteffect = value;
+							return this;
+						}
+					};
+					this.depth = function(value) {
+						if (value === undefined) {
+							return _depth;
+						} else {
+							_depth = value;
+							return this;
+						}
+					};
 					this.corseparador = function(value) {
 						if (value === undefined) {
 							return _corseparador;
@@ -23,7 +62,7 @@ sap.designstudio.sdk.Component
 							return this;
 						}
 					};
-					
+
 					this.angulo = function(value) {
 						if (value === undefined) {
 							return _angulo;
@@ -32,13 +71,13 @@ sap.designstudio.sdk.Component
 							return this;
 						}
 					};
-					
 
 					this.init = function() {
 						// this.$().addClass(STYLE_DIV);
 					};
 
 					this.afterUpdate = function() {
+						this.$().empty();
 						this.getDimension = function() {
 							if (saveDataResultSet != undefined
 									|| saveDataResultSet != null)
@@ -102,6 +141,7 @@ sap.designstudio.sdk.Component
 
 						// PIE CHART
 						chart = new AmCharts.AmPieChart();
+						chart.startEffect = _starteffect;
 						chart.dataProvider = this.getData();
 						chart.titleField = this.getDimension();
 						chart.valueField = this.getMeasure() == "ERROR" ? this
@@ -112,8 +152,19 @@ sap.designstudio.sdk.Component
 						chart.outlineThickness = 2;
 						chart.balloonText = "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>";
 						// this makes the chart 3D
-						chart.depth3D = 15;
+						chart.depth3D = _depth;
 						chart.angle = _angulo;
+
+						chart.addListener("clickSlice", function() {
+							return function(val) {
+								
+								that._clickedText = val.dataItem.title;
+								that._clickedValue = val.dataItem.value;
+								that.firePropertiesChanged( [ "clickedValue",
+										"clickedText" ]);
+								that.fireEvent("onclick");
+							};
+						}());
 
 						// WRITE
 						chart.write(this.$().attr("id"));
